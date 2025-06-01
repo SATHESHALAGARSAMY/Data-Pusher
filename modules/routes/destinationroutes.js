51e4e173f5897fb205b013e08c0ef0aa93f22a3f
SATHESHALAGARSAMY <141311307+SATHESHALAGARSAMY@users.noreply.github.com>
2025-06-01 13:26:07 +0530

    Update destinationroutes.js

--- modules/routes/destinationroutes.js
+++ modules/routes/destinationroutes.js
@@ -3,5 +3,10 @@ const destination = require("../models/destination");
 const router = express.Router();
 
 router.post('/createDestination', destination.createDestination);
+router.get('/getAllDestinations/:accountId', destination.getAllDestinations);
+router.get('/getDestinationById/:destinationId', destination.getDestinationById);
+router.put('/updateDestinationById', destination.updateDestinationById);
+router.post('/deleteDestinationById', destination.deleteDestinationById);
+router.post('/deleteAllDestinationsByAccountId', destination.deleteAllDestinationsByAccountId);
 
 module.exports = router;
