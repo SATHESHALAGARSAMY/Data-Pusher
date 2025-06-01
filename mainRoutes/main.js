7d898e34599c741e68357c65de1c72bb70fade8d
SATHESHALAGARSAMY <141311307+SATHESHALAGARSAMY@users.noreply.github.com>
2025-06-01 13:28:39 +0530

    Update main.js

--- mainRoutes/main.js
+++ mainRoutes/main.js
@@ -2,8 +2,9 @@
 const express = require("express");
 const router = express.Router();
 const account = require("../modules/routes/accountroutes");
+const destination = require("../modules/routes/destinationroutes");
 // Define the POST route for creating an account
 router.use('/account', account);
-
+router.use('/destination', destination);
 // Export the router
 module.exports = router;
